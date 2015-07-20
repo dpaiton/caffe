16f57deb9230d5ac5e912f46340c0c9262f75c03
Dylan Paiton <dpaiton@deeplearn105.flickr.bf1.yahoo.com>
2015-07-20 21:43:06 +0000

    testing commit from server

--- models/sparsenet/logistic/train.sh
+++ models/sparsenet/logistic/train.sh
@@ -3,6 +3,7 @@
 TOOLS=./build/tools
 
 GLOG_logtostderr=0 GLOG_log_dir=models/sparsenet_logistic/ $TOOLS/caffe train \
-  --solver=models/sparsenet_logistic/solver_$1.prototxt
-    #--gpu=$2
+    --solver=models/sparsenet_logistic/solver_$1.prototxt \
+    $2
+    #--gpu=1
     #--weights=models/sparsenet_logistic/sparsenet.caffemodel \
