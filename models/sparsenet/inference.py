6bf5512104b013854ba7df3fa989cef2fbf1ceab
Dylan Paiton <dpaiton@yahoo-inc.com>
2015-07-20 14:01:49 -0700

    made more general for additional analysis

--- models/sparsenet/inference.py
+++ models/sparsenet/inference.py
@@ -5,7 +5,7 @@ from PIL import Image
 from time import time
 from cStringIO import StringIO
 from matplotlib import pyplot as plt
-from os.path import join
+import os
 import sys
 import IPython
 
@@ -19,13 +19,25 @@ parser = ArgumentParser('sgd training driver')
 parser.add_argument('-d', '--device_id', type=int, help='''gpu device number''',
                     default=-1)
 
-root_dir   = '/osx/caffe/'
+root_dir   = '/Users/dpaiton/Code/caffe/'
+exp_lbl    = 'logistic'  # logistic or euclidean
+model_lbl  = 'mlp'       # sparsenet(_deep) or mlp(_deep)
 model_ver  = 'v.0.0'
 mov_start  = 1000
 mov_step   = 1000
-mov_end    = 11000
-model_file = root_dir+'/models/sparsenet_mnist/sparsenet_'+model_ver+'_iter_'+str(mov_end)+'.caffemodel'
-model_prototxt = 'models/sparsenet_mnist/sparsenet.prototxt'
+mov_end    = 5000
+
+weight_layer_name = 'ip1'
+activity_analysis = False 
+pixel_bias        = False
+make_recon        = False
+
+model_file     = root_dir+'/models/sparsenet/'+exp_lbl+'/'+model_lbl+'_'+model_ver+'_iter_'+str(mov_end)+'.caffemodel'
+model_prototxt = 'models/sparsenet/'+exp_lbl+'/'+model_lbl+'.prototxt'
+out_dir        = root_dir+'/models/sparsenet/'+exp_lbl+'/Analysis/'
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
 
 # take an array of shape (n, height, width) or (n, height, width, channels)
 # and visualize each (height, width) thing in a grid of size approx. sqrt(n) by sqrt(n)
@@ -46,29 +58,33 @@ def vis_square(data, padsize=1, padval=0):
 
 def make_movies(start,end,step):
     for iter in range(start,end,step):
-    	model_file = root_dir+'/models/sparsenet_mnist/sparsenet_'+model_ver+'_iter_'+str(iter)+'.caffemodel'
+    	model_file = root_dir+'/models/sparsenet/'+exp_lbl+'/'+model_lbl+'_'+model_ver+'_iter_'+str(iter)+'.caffemodel'
     	net        = caffe.Net(model_prototxt, model_file, caffe.TEST)
 
-    	weights    = np.array(net.params['decode'][0].data)
-        weight_vis = vis_square(weights.T.reshape(weights.shape[1], 28, 28))
+    	weights    = np.array(net.params[weight_layer_name][0].data)
+        weight_len = np.int32(np.sqrt(weights.shape[1]))
+        weight_vis = vis_square(weights.T.reshape(weights.shape[0], weight_len, weight_len))
         weight_img = np.uint8(weight_vis*255)
-        Image.fromarray(weight_img).save(root_dir+'Analysis/weights_'+model_ver+'_'+str(iter)+'.png')
+        Image.fromarray(weight_img).save(out_dir+'/'+weight_layer_name+'_weights_'+model_ver+'_'+str(iter)+'.png')
 	
-	weights_l2 = np.sqrt(np.sum(weights**2,axis=0))
-	plt.bar(np.arange(0,len(weights_l2)),weights_l2)
-	plt.savefig(root_dir+'Analysis/weight_l2_'+model_ver+'_'+str(iter)+'.png',bbox_inches='tight')
-	plt.clf()
+    weights_l2 = np.sqrt(np.sum(weights**2,axis=0))
+    plt.bar(np.arange(0,len(weights_l2)),weights_l2)
+    plt.savefig(out_dir+'/weight_l2_'+model_ver+'_'+str(iter)+'.png',bbox_inches='tight')
+    plt.clf()
 
-	biases   = np.array(net.params['decode'][1].data)
-	bias_vis = vis_square(biases.reshape(1, 28, 28))
-	bias_img = np.uint8(bias_vis*255)
-	Image.fromarray(bias_img).save(root_dir+'Analysis/bias_'+model_ver+'_'+str(iter)+'.png')
+    if pixel_bias:
+        biases   = np.array(net.params[weight_layer_name][1].data)
+        bias_len = np.int32(np.sqrt(biases.shape[1]))
+        bias_vis = vis_square(biases.reshape(1, bias_len, bias_len))
+        bias_img = np.uint8(bias_vis*255)
+        Image.fromarray(bias_img).save(out_dir+'/'+weight_layer_name+'_bias_'+model_ver+'_'+str(iter)+'.png')
 
+    if activity_analysis:
         net.forward()
-	activity = np.array(net.blobs['encode'].data)
+        activity = np.array(net.blobs['encode'].data)
         activity_img = activity / np.max(np.abs(activity)) * 255./2 + 255./2
-	activity_img = np.uint8(activity_img)
-	Image.fromarray(activity_img).save(root_dir+'Analysis/activity_'+model_ver+'_'+str(iter)+'.png')
+        activity_img = np.uint8(activity_img)
+        Image.fromarray(activity_img).save(out_dir+'Analysis/activity_'+model_ver+'_'+str(iter)+'.png')
 
 	#IPython.embed()
 
@@ -94,25 +110,26 @@ def main(args):
 
     net.forward()
     input_dat = np.squeeze(np.array(net.blobs['data'].data))
-    recon     = np.array(net.blobs['decode'].data).reshape(input_dat.shape)
-
     input_vis = vis_square(input_dat)
     input_img = np.uint8(input_vis*255)
-    Image.fromarray(input_img).save(root_dir+'Analysis/input_img_'+model_ver+'.png')
+    Image.fromarray(input_img).save(out_dir+'/input_img_'+model_ver+'.png')
 
-    recon_vis = vis_square(recon)
-    recon_img = np.uint8(recon_vis*255)
-    Image.fromarray(recon_img).save(root_dir+'Analysis/recon_'+model_ver+'.png')
+    if make_recon:
+        recon     = np.array(net.blobs[weight_layer_name].data).reshape(input_dat.shape)
+        recon_vis = vis_square(recon)
+        recon_img = np.uint8(recon_vis*255)
+        Image.fromarray(recon_img).save(out_dif+'/recon_'+model_ver+'.png')
 
     make_movies(mov_start,mov_end+100,mov_step)
 
-    activity = []
-    activity.append(np.array(net.blobs['encode'].data))
-    for iter in range(50):
-        net.forward()
+    if activity_analysis:
+        activity = []
         activity.append(np.array(net.blobs['encode'].data))
-    plt.hist(np.vstack(activity).flatten(),bins=1000)
-    plt.savefig(root_dir+'Analysis/activity_hist_'+model_ver+'.png',bbox_inches='tight')
+        for iter in range(50):
+            net.forward()
+            activity.append(np.array(net.blobs['encode'].data))
+        plt.hist(np.vstack(activity).flatten(),bins=1000)
+        plt.savefig(out_dir+'/activity_hist_'+model_ver+'.png',bbox_inches='tight')
 
     #IPython.embed()
 
