e26e5a9b9df45b2dd15c8ced0dd6a7cee76c1587
Dylan Paiton <dpaiton@yahoo-inc.com>
2015-07-20 12:47:09 -0700

    new folder scheme to make workflow easier

--- models/sparsenet/euclidean/train.sh
+++ models/sparsenet/euclidean/train.sh
@@ -3,6 +3,6 @@
 TOOLS=./build/tools
 
 GLOG_logtostderr=0 GLOG_log_dir=models/sparsenet_mnist/ $TOOLS/caffe train \
-  --solver=models/sparsenet_mnist/solver.prototxt
+    --solver=models/sparsenet_mnist/solver.prototxt
+    #--weights=models/sparsenet_mnist/sparsenet.caffemodel
     #--gpu=$1
-    #--weights=models/sparsenet_mnist/sparsenet.caffemodel \
