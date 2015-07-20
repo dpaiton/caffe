5ff210c8610173679b8b22ca47e2be22d35a29bd
Dylan Paiton <dpaiton@yahoo-inc.com>
2015-07-20 12:27:45 -0700

    made train general so we don't need many copies

--- models/sparsenet_logistic/train_deep.sh
+++ models/sparsenet_logistic/train_deep.sh
@@ -3,6 +3,6 @@
 TOOLS=./build/tools
 
 GLOG_logtostderr=0 GLOG_log_dir=models/sparsenet_logistic/ $TOOLS/caffe train \
-    --solver=models/sparsenet_logistic/solver.prototxt \
-    --gpu=$1
+    --solver=models/sparsenet_logistic/solver_$1.prototxt \
+    --gpu=$2
     #--weights=models/sparsenet_logistic/sparsenet_v.0.0_iter_18000.caffemodel \
