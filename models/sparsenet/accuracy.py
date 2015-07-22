93706606d4299700ef5033d7d4010d61270afc5b
Dylan Paiton <dpaiton@yahoo-inc.com>
2015-07-22 14:18:53 -0700

    couple of new analysis scripts

---
+++ models/sparsenet/accuracy.py
@@ -0,0 +1,81 @@
+import caffe
+import matplotlib.pyplot as plt
+import numpy as np
+
+caffe_root     = '/Users/dpaiton/Code/caffe/'
+model_list     = ['sparsenet', 'mlp']
+version_list   = [80, 90, 95, 99]
+iteration_list = range(10000,50001,10000)
+model_num      = 3
+
+iteration      = iteration_list[-1]
+
+caffe.set_mode_cpu()
+
+lbl_err = np.zeros((len(model_list),len(version_list)))
+
+for midx, model in enumerate(model_list):
+    for vidx, version in enumerate(version_list):
+        model_file = caffe_root+'models/sparsenet/logistic/checkpoints/'+model+ \
+                    '_v.'+str(version)+'.'+str(model_num)+'_iter_'+str(iteration)+'.caffemodel'
+
+        net_proto  = caffe_root+'models/sparsenet/logistic/'+model+'.prototxt'
+
+        net = caffe.Net(net_proto, model_file, caffe.TEST)
+
+        lbl_accuracy = 0
+
+        for iter in range(100):
+            out = net.forward()
+            lbl_accuracy += out['accuracy']
+
+        lbl_accuracy /= 100.
+
+        lbl_err[midx,vidx] = 1 - lbl_accuracy
+
+version = version_list[-2]
+
+itr_err = np.zeros((len(model_list),len(iteration_list)))
+
+for midx, model in enumerate(model_list):
+    for iidx, iteration in enumerate(iteration_list):
+        model_file = caffe_root+'models/sparsenet/logistic/checkpoints/'+model+ \
+                    '_v.'+str(version)+'.'+str(model_num)+'_iter_'+str(iteration)+'.caffemodel'
+
+        net_proto  = caffe_root+'models/sparsenet/logistic/'+model+'.prototxt'
+
+        net = caffe.Net(net_proto, model_file, caffe.TEST)
+
+        itr_accuracy = 0
+
+        for iter in range(100):
+            out = net.forward()
+            itr_accuracy += out['accuracy']
+
+        itr_accuracy /= 100.
+
+        itr_err[midx,iidx] = 1 - itr_accuracy
+
+
+print model_list[0]+" lbl err: "+str(lbl_err[0,:])
+print model_list[1]+" lbl err: "+str(lbl_err[1,:])
+print "\n"
+print model_list[0]+" itr err: "+str(itr_err[0,:])
+print model_list[1]+" itr err: "+str(itr_err[1,:])
+
+plt.figure(1)
+plt.plot(version_list, lbl_err[0,:], 'r', label='SA')
+plt.plot(version_list, lbl_err[1,:], 'b', label='MLP')
+plt.xlabel('% of labels set to ignore')
+plt.ylabel('error rate')
+plt.title('Error Rate with Diminishing Labels')
+plt.legend(loc='upper left')#bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+plt.figure(2)
+plt.plot(iteration_list, itr_err[0,:], 'r', label='SA')
+plt.plot(iteration_list, itr_err[1,:], 'b', label='MLP')
+plt.xlabel('iteration #')
+plt.ylabel('error rate')
+plt.title('Error Rate with '+str(100-version)+' % Labels')
+plt.legend(loc='upper left')#bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.show()
---
+++ models/sparsenet/count_labels.py
@@ -0,0 +1,50 @@
+import caffe
+import numpy as np
+import lmdb
+import random
+
+dataset_dir    = 'examples/mnist/'
+percent_ignore = 80
+new_dataset    = dataset_dir+'/mnist_train_lmdb_'+str(percent_ignore)+'/'
+
+env = lmdb.open(new_dataset, readonly=True)
+
+# txn is a Transaction object
+datum = caffe.proto.caffe_pb2.Datum()
+
+# read once to get dim
+label_list = []
+
+# loop through dataset once to get labels
+with env.begin() as txn:
+    lmdb_cursor = txn.cursor()
+    for key, value in lmdb_cursor:
+        datum.ParseFromString(value)
+        label_list.append(datum.label)
+
+dataset_size = len(label_list)
+
+list0 = [idx for idx,val in enumerate(label_list) if val==0]
+list1 = [idx for idx,val in enumerate(label_list) if val==1]
+list2 = [idx for idx,val in enumerate(label_list) if val==2]
+list3 = [idx for idx,val in enumerate(label_list) if val==3]
+list4 = [idx for idx,val in enumerate(label_list) if val==4]
+list5 = [idx for idx,val in enumerate(label_list) if val==5]
+list6 = [idx for idx,val in enumerate(label_list) if val==6]
+list7 = [idx for idx,val in enumerate(label_list) if val==7]
+list8 = [idx for idx,val in enumerate(label_list) if val==8]
+list9 = [idx for idx,val in enumerate(label_list) if val==9]
+listIg = [idx for idx,val in enumerate(label_list) if val==-1]
+
+print '-1: '+str(len(listIg))
+print '0: '+str(len(list0))
+print '1: '+str(len(list1))
+print '2: '+str(len(list2))
+print '3: '+str(len(list3))
+print '4: '+str(len(list4))
+print '5: '+str(len(list5))
+print '6: '+str(len(list6))
+print '7: '+str(len(list7))
+print '8: '+str(len(list8))
+print '9: '+str(len(list9))
+print 'total: '+str(len(listIg)+len(list0)+len(list1)+len(list2)+len(list3)+len(list4)+len(list5)+len(list6)+len(list7)+len(list8)+len(list9))
