e4e03f27d7dcb626a0bd7480e4c43506370ea6ef
Dylan Paiton <dpaiton@yahoo-inc.com>
2015-08-13 21:05:11 +0000

    param changes for server run

--- models/sparsenet/euclidean/train.sh
+++ models/sparsenet/euclidean/train.sh
@@ -4,6 +4,6 @@ TOOLS=./build/tools
 
 GLOG_logtostderr=0 GLOG_log_dir=models/sparsenet/euclidean/ $TOOLS/caffe train \
     --solver=models/sparsenet/euclidean/solver_$1.prototxt \
+    --weights=models/sparsenet/euclidean/sparsenet_v.30.0_iter_1000000.caffemodel \
     $2
     #--gpu=1
-    #--weights=models/sparsenet/euclidean/sparsenet.caffemodel
