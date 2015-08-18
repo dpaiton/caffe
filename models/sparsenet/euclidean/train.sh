d8c5fe44c9420135e35d67396c23a2630ddef5ee
Dylan Paiton <dpaiton@yahoo-inc.com>
2015-08-18 21:32:13 +0000

    params for server runs

--- models/sparsenet/euclidean/train.sh
+++ models/sparsenet/euclidean/train.sh
@@ -4,6 +4,6 @@ TOOLS=./build/tools
 
 GLOG_logtostderr=0 GLOG_log_dir=models/sparsenet/euclidean/ $TOOLS/caffe train \
     --solver=models/sparsenet/euclidean/solver_$1.prototxt \
-    --weights=models/sparsenet/euclidean/sparsenet_v.30.0_iter_1000000.caffemodel \
     $2
+    #--weights=models/sparsenet/euclidean/sparsenet_v.30.0_iter_1000000.caffemodel \
     #--gpu=1
--- models/sparsenet/logistic/train.sh
+++ models/sparsenet/logistic/train.sh
@@ -4,6 +4,6 @@ TOOLS=./build/tools
 
 GLOG_logtostderr=0 GLOG_log_dir=models/sparsenet/logistic/ $TOOLS/caffe train \
     --solver=models/sparsenet/logistic/solver_$1.prototxt \
+    --weights=models/sparsenet/euclidean/sparsenet_v.34.0_iter_20000.caffemodel \
     $2
     #--gpu=1
-    #--weights=models/sparsenet/logistic/sparsenet.caffemodel 
