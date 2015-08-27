6c8a2bcab7efd2919a5ce83b6a3c065e674c1824
Dylan Paiton <dpaiton@deeplearn104.flickr.bf1.yahoo.com>
2015-08-27 20:52:23 +0000

    latest params

--- models/sparsenet/logistic/train.sh
+++ models/sparsenet/logistic/train.sh
@@ -4,6 +4,6 @@ TOOLS=./build/tools
 
 GLOG_logtostderr=0 GLOG_log_dir=models/sparsenet/logistic/ $TOOLS/caffe train \
     --solver=models/sparsenet/logistic/solver_$1.prototxt \
-    --weights=models/sparsenet/euclidean/sparsenet_v.34.0_iter_20000.caffemodel \
     $2
+    #--weights=models/sparsenet/euclidean/sparsenet_v.33.0_iter_2000000.caffemodel \
     #--gpu=1
