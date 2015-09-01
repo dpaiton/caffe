ae092a6d08d48a06579a56888d2d4111e4f654a6
Dylan Paiton <dpaiton@yahoo-inc.com>
2015-09-01 11:59:23 -0700

    fixed bug for 0-valued entries

--- models/sparsenet/plot_energy.py
+++ models/sparsenet/plot_energy.py
@@ -1,106 +1,87 @@
-import matplotlib
-matplotlib.use('Agg')
+#import matplotlib
+#matplotlib.use('Agg')
 import caffe
 import numpy as np
 import os
-from argparse import ArgumentParser
+import glob
 import re
 import matplotlib.pyplot as plt
 import IPython
 
 np.random.seed(0)
 
-parser = ArgumentParser('sgd training driver')
-parser.add_argument('-d', '--device_id', type=int, help='''gpu device number''',
-                    default=-1)
-
-#root_dir   = '/Users/dpaiton/Code/caffe/'
-root_dir   = '/nfs/dylan/caffe/'
+root_dir   = '/Users/dpaiton/Code/caffe/'
+#root_dir   = '/nfs/dylan/caffe/'
 exp_lbl    = 'logistic'  # logistic or euclidean
 model_lbl  = 'sparsenet' # sparsenet or mlp
-model_ver  = 'v.100.9'
-mov_start  = 1000
-mov_step   = 1000
-mov_end    = 70000
+model_ver  = 'v.100.7'
 
-#model_pretxt    = root_dir+'/models/sparsenet/'+exp_lbl+'/checkpoints/'+model_lbl+'_'+model_ver+'_iter_'
-model_pretxt    = root_dir+'/models/sparsenet/'+exp_lbl+'/'+model_lbl+'_'+model_ver+'_iter_'
-model_prototxt  = root_dir+'/models/sparsenet/'+exp_lbl+'/'+model_lbl+'_deep.prototxt'
+def main():
+    out_dir = root_dir+'/models/sparsenet/'+exp_lbl+'/Analysis/'
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
-if exp_lbl is 'logistic':
-    solver_prototxt = root_dir+'/models/sparsenet/'+exp_lbl+'/solver_sparse_deep.prototxt'
-elif exp_lbl is 'euclidean':
-    solver_prototxt = root_dir+'/models/sparsenet/'+exp_lbl+'/solver.prototxt'
-else:
-    raise ValueError('exp_lbl must be either "logistic" or "euclidean"')
+    log_dir = root_dir+'models/sparsenet/'+exp_lbl+'/logfiles/'+model_lbl+'*'
+    files = glob.glob(log_dir)
+    log_file = [fi for fi in files if model_ver in fi][0]
 
-out_dir = root_dir+'/models/sparsenet/'+exp_lbl+'/Analysis/'
+    log_text = open(log_file,'r').read()
 
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
+    max_iter = float(re.findall("max_iter: (\d+)",log_text)[0])
 
-def main(args):
-    if args.device_id == -1:
-        print 'running on cpu'
-        caffe.set_mode_cpu()
-    else:
-        print 'running on gpu %d' % args.device_id
-        caffe.set_device(args.device_id)
-        caffe.set_mode_gpu()
+    test_start = 0
+    test_step = float(re.findall("test_interval: (\d+)",log_text)[0])
+    test_times = np.arange(test_start,max_iter+1,test_step)[1:]
 
-    # time steps
-    model_times = np.arange(mov_start,mov_end+1,mov_step)
+    train_start = 0
+    train_step = float(re.findall("display: (\d+)",log_text)[0])
+    train_times = np.arange(train_start,max_iter,train_step)[1:]
 
-    # get lambda value
-    if exp_lbl is 'logistic':
-        num_labels = re.findall('v\.(\d+)\.',model_ver)[0]
-    text = open(model_prototxt,'r').read()
-    lamb = float(re.findall("lambda\: (\d+.\d+)", text)[0])
-    batch_size = float(re.findall("batch_size: (\d+)",text)[0])
+    loss_types = re.findall('type\: \"(\w+)Loss\"', log_text)
+    loss_vals = [float(val) for val in re.findall('loss_weight\: (\d+\.?[\d?]+)', log_text)]
+    if not loss_vals: #it's possible that it wasn't specified
+        loss_vals = [1.0 for type in loss_types]
 
-    accuracy_vals  = np.zeros((len(model_times)))
-    energy_vals    = np.zeros((len(model_times)))
+    loss_pairs = dict(zip(loss_types,loss_vals))
 
-    # prepare solver
-    solver = caffe.get_solver(solver_prototxt)
+    tmp_list = [float(val) for val in re.findall('Train net output \#\d\: euclidean_loss \= (\d+\.?[\d?]+)',log_text)[1:]]
+    euclidean_list = tmp_list if tmp_list else [0.0 for time in train_times]
+    tmp_list = [float(val) for val in re.findall('Train net output \#\d\: softmax_loss \= (\d+\.?[\d?]+)',log_text)[1:]]
+    softmax_list = tmp_list if tmp_list else [0.0 for time in train_times]
 
-    index = 0
-    for model_itr in model_times:
-        model_file = model_pretxt+str(model_itr)+'.caffemodel'
-        solver.net.copy_from(model_file)
-        solver.step(1)
+    soft_mult = 1.0
+    euc_mult = 1.0
+    if 'SoftmaxWith' in loss_pairs.keys():
+        soft_mult = loss_pairs['SoftmaxWith']
+    if 'Euclidean' in loss_pairs.keys():
+        euc_mult = loss_pairs['Euclidean']
 
-	
-        for j in range(10):
-	    scores_train = solver.net.forward()
-            energy_vals[index] += 0.9*scores_train['softmax_loss'] + 0.1*scores_train['euclidean_loss']
+    energy_vals = np.array([euc_mult*a + soft_mult*b for a,b in zip(euclidean_list,softmax_list)])
+    accuracy_vals = np.array([float(val) for val in re.findall('Test net output \#\d\: accuracy \= (\d+\.?[\d?]+)',log_text)[1:]])
+    lr_vals = np.array([float(val) for val in re.findall('lr \= (\d+\.?[\d?]+)',log_text)])[1:]
 
-        energy_vals[index] /= float(10)
-
-        for i in range(100):
-            scores_test = solver.test_nets[0].forward()
-            accuracy_vals[index] += scores_test['accuracy']
-
-	accuracy_vals[index] /= 100
-
-        index += 1
+    if np.max(energy_vals) > 0:
+        energy_vals /= np.max(energy_vals)
+    if np.max(lr_vals) > 0:
+        lr_vals /= np.max(lr_vals)
 
     fig, ax1 = plt.subplots()
-    ax1.plot(model_times, energy_vals, 'b',label='energy')
+    ax1.plot(train_times, energy_vals, 'b',label='energy')
+    #ax1.plot(train_times, lr_vals, 'g',label='learning rate')
     ax1.set_ylabel('Energy')
     ax1.set_xlabel('Model Time Step')
     ax2 = ax1.twinx()
-    ax2.plot(model_times,accuracy_vals,'r',label='accuracy')
+    ax2.plot(test_times,accuracy_vals,'r',label='accuracy')
     ax2.set_ylabel('Accuracy')
     if exp_lbl is 'logistic':
+        num_labels = re.findall('v\.(\d+)\.',model_ver)[0]
         plt.title('Energy analysis for '+num_labels+' training labels')
     else:
         plt.title('Energy analysis')
-    #legend = fig.legend(loc='best', shadow=False, fontsize='small')
-    plt.savefig(out_dir+'/energy_'+model_ver+'.png',dpi=1000,bbox_inches='tight')
+    legend = plt.legend(loc='best', shadow=False, fontsize='small')
+    plt.savefig(out_dir+'/energy_'+model_lbl+'_'+model_ver+'.png',dpi=1000,bbox_inches='tight')
 
     #IPython.embed()
 
 if __name__ == '__main__':
-    args = parser.parse_args()
-    main(args)
+    main()
