ff0d733f45fc9e247da0739ea05cb69f7ffe3914
Dylan Paiton <dpaiton@yahoo-inc.com>
2015-09-04 15:38:35 -0700

    unit forward test written, but not working
    Not sure what is going on, but I think the way I'm iterating
    the unit layer is not correct.

--- src/caffe/test/test_sparse_layers.cpp
+++ src/caffe/test/test_sparse_layers.cpp
@@ -25,12 +25,12 @@ extern cudaDeviceProp CAFFE_TEST_CUDA_PROP;
  * positive only to make sure energy decreases.
  **/
 template <typename TypeParam>
-class SparseUnitLayerTest : public MultiDeviceTest<TypeParam> {
+class SparseLayerTest : public MultiDeviceTest<TypeParam> {
   typedef typename TypeParam::Dtype Dtype;
 
  protected:
   //create blob -> batch=3; channels=3 (RGB); pixels = 4 (2x2)
-  SparseUnitLayerTest()
+  SparseLayerTest()
       : blob_bottom_0_(new Blob<Dtype>(4, 3, 2, 2)),
         blob_bottom_1_(new Blob<Dtype>(4, 10, 1, 1)),
         blob_top_(new Blob<Dtype>()) {
@@ -52,42 +52,47 @@ class SparseUnitLayerTest : public MultiDeviceTest<TypeParam> {
     blob_top_vec_.push_back(blob_top_);
   }
 
-  virtual ~SparseUnitLayerTest() { delete blob_bottom_0_; delete blob_bottom_1_; delete blob_top_; }
+  virtual ~SparseLayerTest() { delete blob_bottom_0_; delete blob_bottom_1_; delete blob_top_; }
 
   Dtype compute_energy(shared_ptr<SparseUnitLayer<Dtype> > layer, LayerParameter layer_param){
-    // E = 1/2 sum_p( (x[p] - sum_m(phi[m,p] * a[m]) - b[p])^2 ) + lambda * sum_m(a[m])
+    // E = 1/2 sum_p( (x[p] - sum_m(phi[m,p] * a[m]) - b[p])^2 ) + lambda_ * sum_m(a[m])
+    Dtype lambda_    = layer_param.mutable_sparse_unit_param()->lambda();
+    Dtype bias_term_ = layer_param.mutable_sparse_unit_param()->bias_term(); 
     const Blob<Dtype>* input    = this->blob_bottom_vec_[0];
     const Blob<Dtype>* activity = this->blob_top_vec_[0];
-    const shared_ptr<Blob<Dtype> >  weights = layer->blobs()[0];
-    const shared_ptr<Blob<Dtype> >  bias    = layer->blobs()[1];
+    const shared_ptr<Blob<Dtype> > weights = layer->blobs()[0];
     int batch_size   = this->blob_bottom_vec_[0]->shape(0); // M_
     int num_channels = this->blob_bottom_vec_[0]->shape(1);
     int num_pixelsH  = this->blob_bottom_vec_[0]->shape(2); // H
     int num_pixelsW  = this->blob_bottom_vec_[0]->shape(3); // W
-    int num_elements = this->blob_bottom_vec_[0]->count(1); // M
-    Dtype lambda     = layer_param.mutable_sparse_unit_param()->lambda();
+    int num_elements = this->blob_bottom_vec_[1]->count(1); // M
+    std::cout<<"\nnum_elements: "<<num_elements<<"\n";
     Dtype E = 0;
     for (int b=0; b < batch_size; ++b) {                    // batch
-        Dtype residual_err = 0;
-        Dtype a_sum = 0;
-        for (int c=0; c < num_channels; ++c) {              // channel
-            for (int h=0; h < num_pixelsH; ++h) {           // height
-                for (int w=0; w < num_pixelsW; ++w) {       // width
-                    Dtype inner_term = 0;
-                    for (int m=0; m < num_elements; ++m) {  // elements
-                        if (c==0 && h==0 && w==0) {
-                            a_sum += std::abs(activity->cpu_data()[activity->offset(b, m)]);
-                        }
-                        inner_term += input->cpu_data()[input->offset(b, c, h, w)] - 
-                                      weights->cpu_data()[weights->offset(m, c*h*w+h*w+w)] * 
-                                      activity->cpu_data()[activity->offset(b, m)] - 
-                                      bias->cpu_data()[c*h*w+h*w+w];
-                    }
-                    residual_err += inner_term * inner_term;
-                }
+      Dtype residual_err = 0;
+      Dtype a_sum = 0;
+      for (int c=0; c < num_channels; ++c) {              // channel
+        for (int h=0; h < num_pixelsH; ++h) {           // height
+          for (int w=0; w < num_pixelsW; ++w) {       // width
+            Dtype inner_term = 0;
+            for (int m=0; m < num_elements; ++m) {  // elements
+              if (c==0 && h==0 && w==0) {
+                a_sum += std::abs(activity->cpu_data()[activity->offset(b, m)]);
+              }
+              inner_term += input->cpu_data()[input->offset(b, c, h, w)] - 
+                  weights->cpu_data()[weights->offset(m, c*h*w+h*w+w)] * 
+                  activity->cpu_data()[activity->offset(b, m)];
+              if (bias_term_) {
+                inner_term -= layer->blobs()[1]->cpu_data()[c*h*w+h*w+w];
+              }
             }
+            residual_err += inner_term * inner_term;
+          }
         }
-        E += 0.5 * residual_err + lambda * a_sum;
+      }
+      E += 0.5 * residual_err + lambda_ * a_sum;
+      std::cout<<"a_sum: "<<a_sum<<"\n";
+      std::cout<<"residual_error: "<<residual_err<<"\n";
     }
     return E;
   }
@@ -99,9 +104,9 @@ class SparseUnitLayerTest : public MultiDeviceTest<TypeParam> {
   vector<Blob<Dtype>*> blob_top_vec_;
 };
 
-TYPED_TEST_CASE(SparseUnitLayerTest, TestDtypesAndDevices);
+TYPED_TEST_CASE(SparseLayerTest, TestDtypesAndDevices);
 
-TYPED_TEST(SparseUnitLayerTest, TestSetUp) {
+TYPED_TEST(SparseLayerTest, TestUnitSetUp) {
 
   typedef typename TypeParam::Dtype Dtype;
   LayerParameter layer_param;
@@ -115,7 +120,68 @@ TYPED_TEST(SparseUnitLayerTest, TestSetUp) {
   EXPECT_EQ(this->blob_top_->shape(1), 10); // N_ -> Elements
 }
 
-//TYPED_TEST(SparseUnitLayerTest, TestForward) {
+//TYPED_TEST(SparseLayerTest, TestUnitForward) {
+//  typedef typename TypeParam::Dtype Dtype;
+//  bool IS_VALID_CUDA = false;
+//  #ifndef CPU_ONLY
+//  IS_VALID_CUDA = CAFFE_TEST_CUDA_PROP.major >= 2;
+//  #endif
+//  
+//  if (Caffe::mode() == Caffe::CPU ||
+//      sizeof(Dtype) == 4 || IS_VALID_CUDA) {
+//    LayerParameter layer_param;
+//    SparseUnitParameter* sparse_unit_param =
+//        layer_param.mutable_sparse_unit_param();
+//
+//    sparse_unit_param->set_lambda(0.1);
+//    sparse_unit_param->set_eta(0.01);
+//    sparse_unit_param->mutable_weight_filler()->set_type("uniform");
+//    sparse_unit_param->mutable_weight_filler()->set_min(0);
+//    sparse_unit_param->mutable_weight_filler()->set_max(0.1);
+//    sparse_unit_param->set_bias_term(false);
+//    sparse_unit_param->mutable_bias_filler()->set_type("uniform");
+//    sparse_unit_param->mutable_bias_filler()->set_min(0);
+//    sparse_unit_param->mutable_bias_filler()->set_max(0.5);
+//
+//    shared_ptr<SparseUnitLayer<Dtype> > layer(
+//        new SparseUnitLayer<Dtype>(layer_param));
+//
+//    layer->SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+//
+//    Dtype E1 = this->compute_energy(layer,layer_param);
+//    std::cout<<"Energy check E1: "<<E1<<"\n";
+//
+//    std::cout<<"\n--t=0--\n";
+//    layer->Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+//    Dtype Eng = this->compute_energy(layer,layer_param);
+//
+//    std::cout<<"Energy check t=0 E="<<Eng<<"\n";
+//    //CHECK_LE(Eng,E1)
+//
+//    vector<Blob<Dtype>*> new_blob_bottom_vec_;
+//    for (int t = 1; t < 3; ++t) { 
+//        std::cout<<"\n--t="<<t<<"--\n";
+//        new_blob_bottom_vec_.clear();
+//        new_blob_bottom_vec_.push_back(this->blob_bottom_vec_[0]);
+//        new_blob_bottom_vec_.push_back(this->blob_top_vec_[0]);
+//        layer->Forward(new_blob_bottom_vec_, this->blob_top_vec_);
+//        //this->blob_bottom_vec_[1] = this->blob_top_vec_[0];
+//        //layer->Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+//        Eng = this->compute_energy(layer,layer_param);
+//        std::cout<<"Energy check t="<<t<<" E="<<Eng<<"\n";
+//    }
+//
+//    //Dtype E2 = this->compute_energy(layer,layer_param);
+//    //CHECK_LE(E2,E1);
+//    CHECK_LE(Eng,E1);
+//  }
+//}
+
+//TYPED_TEST(SparseLayerTest, TestUnitGradient) {
+//}
+
+
+//TYPED_TEST(SparseLayerTest, TestForward) {
 //  typedef typename TypeParam::Dtype Dtype;
 //  bool IS_VALID_CUDA = false;
 //#ifndef CPU_ONLY
@@ -175,7 +241,7 @@ TYPED_TEST(SparseUnitLayerTest, TestSetUp) {
 //  }
 //}
 
-//TYPED_TEST(SparseUnitLayerTest, TestGradient) {
+//TYPED_TEST(SparseLayerTest, TestGradient) {
 //  typedef typename TypeParam::Dtype Dtype;
 //  bool IS_VALID_CUDA = false;
 //
@@ -229,4 +295,3 @@ TYPED_TEST(SparseUnitLayerTest, TestSetUp) {
 //}
 
 }  // namespace caffe
-
